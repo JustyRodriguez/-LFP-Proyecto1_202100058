25281cfa0d6957f01d3753e76f77cacec47bef2b
SebastianR <sebastianrodriguezdelcid@gmail.com>
2023-03-23 16:24:34 -0600

    Instrucciones de operaciones

---
+++ Instrucciones/aritmeticas.py
@@ -0,0 +1,36 @@
+from Abstract.abstract import Expression
+
+class Arimetica(Expression):
+    def __init__(self,left,right,tipo, fila, columna):
+        self.left = left
+        self.right = right
+        self.tipo = tipo
+        super().__init__(fila, columna)
+        
+    def operar(self, arbol):
+        leftValue = ''
+        rightValue = ''
+        if self.tipo.operar(arbol) != None:
+            leftValue = self.left.operar(arbol)
+            rightValue = self.right.operar(arbol)
+        elif self.right != None:
+            rightValue = self.right.operar(arbol)
+            
+        if self.tipo.operar(arbol) == 'Suma':
+            return leftValue + rightValue
+        elif self.tipo.operar(arbol) == 'Resta':
+            return leftValue - rightValue
+        elif self.tipo.operar(arbol) == 'Multiplicacion':
+            return leftValue * rightValue
+        elif self.tipo.operar(arbol) == 'Division':
+            return leftValue / rightValue
+        elif self.tipo.operar(arbol) == 'Mod':
+            return leftValue % rightValue
+        elif self.tipo.operar(arbol) == 'Potencia':
+            return leftValue ** rightValue
+        else:
+            return None
+    def getFila(self):
+        return super().getFila()
+    def getColumna(self):
+        return super().getColumna()
